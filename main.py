53d4c35774202380e256fb4b0dfa785f117a3d7d
Vexyenv <vexyenv@outlook.com>
2025-06-04 23:17:48 +0530

    Weather [v1.0.1] (Updated README.md), Added functions for different actions

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 print("1. Celcius to Farenheit")
 print("2. Farenheit to Celcius")
 
-choice = int(input("Choose an option [1 - 2]: "))
+def main():
+  choice = int(input("Choose an option [1 - 2]: "))
+  if choice == 1:
+    celToFaren()
+  elif choice == 2:
+    farenToCel()
+  else:
+    print("Invalid Choice")
 
-if choice == 1:
+def celToFaren():
   cel = float(input("Enter value in Celcius: "))
   faren = (cel * 9 / 5) + 32
-  print(faren, "° Farenheit")
-elif choice == 2:
+  print(f"{faren}° Farenheit")
+
+def farenToCel():
   faren = float(input("Enter value in Farenheit: "))
   cel = (faren - 32) * 5 / 9
-  print(cel, "° Celcius")
-else:
-  print("Invalid Choice")
+  print(f"{cel}° Celcius")
+
+main()
